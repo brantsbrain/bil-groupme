aab12c3bde6068a3d60eb20763680e23b846deae
Brant Goings <bgraulph@gmail.com>
2022-05-27 09:43:19 -0400

    First push

---
+++ bot.js
@@ -0,0 +1,94 @@
+////////// IMPORTS //////////
+const cool = require('cool-ascii-faces')
+const {
+  helptext, helpregex,
+  everyoneregex, mentionEveryone,
+  ballersregex, mentionBallers,
+  getAdmins, postPic,
+  coolregex, createPost
+} = require("./groupme-api")
+
+////////// INITIALIZE VARS //////////
+// Declare quiet boolean
+let quiet = true
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+////////// RESPOND //////////
+const respond = async (req, res) => {
+  try {
+    const request = req.body
+    const requesttext = request.text
+    const senderid = request.user_id
+    const sendername = request.name
+    const notadmin = `You ain't the boss of me, ${sendername}!`
+    console.log(`User request: "${requesttext}"`)
+    console.log(`Request Body: "${JSON.stringify(request)}"`)
+
+    // If text exists
+    if (requesttext) {
+      res.writeHead(200)
+      await sleep(1500)
+
+      ////////// BASE CONTROLS //////////
+
+      // Post a cool face
+      if (coolregex.test(requesttext)) {
+        await createCoolFaceMessage()
+      }
+
+      // Post help text
+      else if (helpregex.test(requesttext)) {
+        await createPost(helptext)
+      }
+
+      ////////// ADMIN CONTROLS //////////
+      // Only allow admins to mention everyone
+      else if (everyoneregex.test(requesttext)) {
+        let adminarr = await getAdmins()
+        if (adminarr.indexOf(senderid) > -1) {
+          await mentionEveryone(requesttext)
+        }
+        else {
+          console.log(`${sendername} attempted to mention everybody`)
+        }
+      }
+
+      // Only allow admins to mention ballers
+      else if (ballersregex.test(requesttext)) {
+        let adminarr = await getAdmins()
+        if (adminarr.indexOf(senderid) > -1) {
+          await mentionBallers(requesttext)
+        }
+        else {
+          console.log(`${sendername} attempted to mention everybody`)
+        }
+      }
+
+      ////////// NO CONDITIONS MET //////////
+      else {
+        console.log("Just chilling... doing nothing...")
+      }
+
+      res.end()
+    }
+    // Does not match regex
+    else {
+      console.log("Don't care")
+      res.writeHead(200)
+      res.end()
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Create cool face
+const createCoolFaceMessage = async () => {
+  const botResponse = cool()
+  await createPost(botResponse)
+}
+
+exports.respond = respond
---
+++ groupme-api.js
@@ -0,0 +1,296 @@
+////////// IMPORTS //////////
+require("dotenv").config()
+const got = require("got")
+const { URL } = require("url")
+const https = require("https")
+
+////////// INITIALIZE VARS //////////
+const baseurl = "https://api.groupme.com/"
+const helptext = "Anurag Commands:\n" +
+                  "/everyone - Mentions everybody in the group (admins only)\n" +
+                  "/ballers - Mention all people going to nearest upcoming event (admin only)\n" +
+                  "/help - Uhhh... you're here"
+
+////////// ENVIRONMENT VARS //////////
+// Required
+const bot_id = process.env.BOT_ID
+const accesstoken = process.env.ACCESS_TOKEN
+const groupid = process.env.GROUP_ID
+
+////////// CHECK ENV VARS //////////
+if (!accesstoken) {
+    console.log("ENV: 'ACCESS_TOKEN' is undefined")
+}
+if (!groupid) {
+    console.log("ENV: 'GROUP_ID' is undefined")
+}
+if (!bot_id) {
+    console.log("ENV: 'BOT_ID' is undefined")
+}
+
+////////// FUNCTIONS/METHODS //////////
+// Tell the bot to create a post within its group
+const createPost = async (message) => {
+    console.log(`Creating new post (${message.length}): ${message}`)
+    const postPath = "/v3/bots/post"
+    const desturl = new URL(postPath, baseurl)
+
+    const response = await got.post(desturl, {
+        json: {
+            "bot_id": bot_id,
+            "text": String(message),
+        },
+    })
+
+    const statusCode = response.statusCode
+    if (statusCode !== 201) {
+        console.log(`Error creating a post ${statusCode}`)
+    }
+}
+
+// Get members
+const getEveryone = async () => {
+  const getpath = `/v3/groups/${groupid}?token=${accesstoken}`
+  const desturl = new URL(getpath, baseurl)
+  const response = await got(desturl, {
+      responseType: "json"
+  })
+
+  memberdict = response.body.response.members
+  console.log(memberdict)
+  let memberarr = []
+  for (const key of Object.entries(memberdict)) {
+    memberarr.push(key[1].user_id)
+  }
+
+  return memberarr
+}
+
+// Get members from the nearest upcoming event
+const getBallers = async () => {
+  const limit = 3
+  const date = new Date().getTime()
+  const yesterdaylong = date - 24*60*60*1000
+  const yesterday = new Date(yesterdaylong)
+  var end_at = yesterday.toISOString()
+  console.log(`end_at set to: ${end_at}`)
+  const getpath = `/v3/conversations/${groupid}/events/list?end_at=${end_at}&limit=${limit}&token=${accesstoken}`
+  const desturl = new URL(getpath, baseurl)
+  const response = await got(desturl, {
+      responseType: "json"
+  })
+
+  console.log(response.body.response)
+
+  // memberarr = response.body.response.events[0].going
+  const eventarr = response.body.response.events
+  let goodevent = []
+
+  for (var i = 0; i < eventarr.length; i++) {
+    if ("deleted_at" in eventarr[i]) {
+      console.log(`Found deleted_at in ${JSON.stringify(eventarr[i])}`)
+    }
+    else {
+      goodevent = eventarr[i]
+      console.log(`Found good event: ${JSON.stringify(goodevent)}`)
+      break
+    }
+  }
+
+  const memberarr = goodevent.going
+  console.log(`Mentioning this array: ${memberarr}`)
+
+  return memberarr
+}
+
+// Get admins
+const getAdmins = async () => {
+  const getpath = `/v3/groups/${groupid}?token=${accesstoken}`
+  const desturl = new URL(getpath, baseurl)
+  const response = await got(desturl, {
+      responseType: "json"
+  })
+
+  // Get admin details
+  memberdict = response.body.response.members
+  let adminarr = []
+  for (const key of Object.entries(memberdict)) {
+    if (key[1].roles.indexOf("admin") > -1) {
+      console.log(`Found: ${key[1].roles} - ${key[1].user_id} - ${key[1].nickname}`)
+      adminarr.push(key[1].user_id)
+    }
+  }
+
+  return adminarr
+}
+
+// Create mention post
+const mentionEveryone = async (slashtext) => {
+  console.log(`Creating new mention (${slashtext.length}): ${slashtext}`)
+  let text = slashtext.replace("/", "@")
+  const message = {
+      text,
+      bot_id,
+      attachments: [{ loci: [], type: "mentions", user_ids: [] }]
+    }
+
+  // Get member IDs as an array and push to message variable
+  members = await getEveryone()
+  for (let i = 0; i < members.length; i++) {
+    message.attachments[0].loci.push([i, i + 1])
+    message.attachments[0].user_ids.push(members[i])
+  }
+
+  // Prep message as JSON and construct packet
+  const json = JSON.stringify(message)
+  const groupmeAPIOptions = {
+    agent: false,
+    host: "api.groupme.com",
+    path: "/v3/bots/post",
+    port: 443,
+    method: "POST",
+    headers: {
+      "Content-Length": json.length,
+      "Content-Type": "application/json",
+      "X-Access-Token": accesstoken
+    }
+  }
+
+  // Send request
+  const req = https.request(groupmeAPIOptions, response => {
+    let data = ""
+    response.on("data", chunk => (data += chunk))
+    response.on("end", () =>
+      console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+    )
+  })
+  req.end(json)
+}
+
+// Create mention post for people that replied going to the closest event
+const mentionBallers = async (slashtext) => {
+  console.log(`Creating new mention (${slashtext.length}): ${slashtext}`)
+  let text = slashtext.replace("/", "@")
+  const message = {
+      text,
+      bot_id,
+      attachments: [{ loci: [], type: "mentions", user_ids: [] }]
+    }
+
+  // Get member IDs as an array and push to message variable
+  let members = await getBallers()
+  for (let i = 0; i < members.length; i++) {
+    message.attachments[0].loci.push([i, i + 1])
+    message.attachments[0].user_ids.push(members[i])
+  }
+
+  // Prep message as JSON and construct packet
+  const json = JSON.stringify(message)
+  const groupmeAPIOptions = {
+    agent: false,
+    host: "api.groupme.com",
+    path: "/v3/bots/post",
+    port: 443,
+    method: "POST",
+    headers: {
+      "Content-Length": json.length,
+      "Content-Type": "application/json",
+      "X-Access-Token": accesstoken
+    }
+  }
+
+  // Send request
+  const req = https.request(groupmeAPIOptions, response => {
+    let data = ""
+    response.on("data", chunk => (data += chunk))
+    response.on("end", () =>
+      console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+    )
+  })
+  req.end(json)
+}
+
+// Mention anyone who hasn't answered a poll yet
+const noPoll = async(text) => {
+  return
+}
+
+// Post pic from URL
+const postPic = async(text) => {
+  console.log(`Posting pic`)
+  const message = {
+      text,
+      bot_id
+    }
+
+    // Prep message as JSON and construct packet
+    const json = JSON.stringify(message)
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "image/jpeg",
+        "X-Access-Token": accesstoken
+      }
+    }
+
+    // Send request
+    const req = https.request(groupmeAPIOptions, response => {
+      let data = ""
+      response.on("data", chunk => (data += chunk))
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      )
+    })
+    req.end(json)
+}
+
+// Create event
+const createEvent = async() => {
+  return
+}
+
+// Returns all your bots and their info
+const getBots = async () => {
+    const grouppath = `/v3/bots?token=${accesstoken}`
+    const desturl = new URL(grouppath, baseurl)
+    const response = await got(desturl, {
+        responseType: "json"
+    })
+    console.log(response.body.response)
+}
+
+////////// REGEX //////////
+const helpregex = /^(\s)*\/help/i
+const coolregex = /^(\s)*\/cool/i
+const everyoneregex = /^(\s)*\/everyone/i
+const ballersregex = /^(\s)*\/ballers/i
+
+////////// EXPORTS //////////
+
+// Pic vars
+exports.postPic = postPic
+
+// Help vars
+exports.helpregex = helpregex
+exports.helptext = helptext
+
+// Everyone
+exports.everyoneregex = everyoneregex
+exports.getAdmins = getAdmins
+
+// Ballers
+exports.getBallers = getBallers
+exports.ballersregex = ballersregex
+exports.mentionBallers = mentionBallers
+
+// Misc vars
+exports.coolregex = coolregex
+exports.getBots = getBots
+exports.createPost = createPost
+exports.getEveryone = getEveryone
+exports.mentionEveryone = mentionEveryone
