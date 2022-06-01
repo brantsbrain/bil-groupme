328f33c7a57cb6b5f19cb717180bbf2b15488260
Brant Goings <bgraulph@gmail.com>
2022-06-01 14:07:26 -0400

    Update groupme-api.js

--- groupme-api.js
+++ groupme-api.js
@@ -233,10 +233,10 @@ const postPic = async(text) => {
 // Create event
 const createEvent = async(name, loc) => {
   console.log(`Creating ${name} event`)
-  const today = new Date()
-  const day = today.getDay()
-  const start_at = (today.getDate() - day + (day === 0 ? -6 : 2)).toISOString()
-  const end_at = (today.getDate() + 1 - day + (day === 0 ? -6 : 2)).toISOString()
+  var today = new Date(), start_at, end_at, day
+  day = today.getDay()
+  start_at = (today.getDate() - day + (day === 0 ? -6 : 2)).toISOString()
+  end_at = (today.getDate() + 1 - day + (day === 0 ? -6 : 2)).toISOString()
 
   const message = {
       name,
