992da27c3ced00608885de6ee7432ace8e108059
Brant Goings <bgraulph@gmail.com>
2022-06-20 12:58:19 -0400

    Updates

--- groupme-api.js
+++ groupme-api.js
@@ -1,17 +1,17 @@
 ////////// IMPORTS //////////
 require("dotenv").config()
 const got = require("got")
-const {URL} = require("url")
+const { URL } = require("url")
 const https = require("https")
 
 ////////// INITIALIZE VARS //////////
 const baseurl = "https://api.groupme.com/"
 const helptext = "Kobe Commands:\n" +
-                  "/ballers - Mention all people going to nearest upcoming event (admin only)\n" +
-                  "/event[:name:location] - Create an event hardcoded for nearest Tuesday 5:30 - 8:30 PM EST (for now)\n" +
-                  "/soccer - Create soccer event for nearest Tuesday\n" +
-                  "/newbies - Posts sparknotes of BIL stuff (admin-only)\n" +
-                  "/help - Uhhh... you're here"
+  "/ballers - Mention all people going to nearest upcoming event (admin only)\n" +
+  "/event[:name:location] - Create an event hardcoded for nearest Tuesday 5:30 - 8:30 PM EST (for now)\n" +
+  "/soccer - Create soccer event for nearest Tuesday\n" +
+  "/newbies - Posts sparknotes of BIL stuff (admin-only)\n" +
+  "/help - Uhhh... you're here"
 
 ////////// ENVIRONMENT VARS //////////
 // Required
@@ -26,139 +26,156 @@ const newbiestext = process.env.NEWBIES_TEXT
 
 ////////// CHECK ENV VARS //////////
 if (!accesstoken) {
-    console.log("ENV: 'ACCESS_TOKEN' is undefined")
+  console.log("ENV: 'ACCESS_TOKEN' is undefined")
 }
 if (!groupid) {
-    console.log("ENV: 'GROUP_ID' is undefined")
+  console.log("ENV: 'GROUP_ID' is undefined")
 }
 if (!bot_id) {
-    console.log("ENV: 'BOT_ID' is undefined")
+  console.log("ENV: 'BOT_ID' is undefined")
 }
 
 ////////// FUNCTIONS/METHODS //////////
 // Create a post and mention users if ID array is provided
 const createPost = async (message, mentionids) => {
-    console.log(`Creating new post (${message.length}): ${message}`)
-    const postPath = "/v3/bots/post"
-    const desturl = new URL(postPath, baseurl)
+  console.log(`Creating new post (${message.length}): ${message}`)
+  const postPath = "/v3/bots/post"
+  const desturl = new URL(postPath, baseurl)
 
-    // Prep message as array to accomadate long messages 
-    let messagearr = []
-    var currmess = ""
-    for (let i = 0; i < message.length; i++) {
-      if (currmess.length < 999) {
-        currmess += message[i]
-      }
-      else {
-        messagearr.push(currmess)
-        currmess = ""
-      }
+  // Prep message as array to accomadate long messages 
+  let messagearr = []
+  var currmess = ""
+  for (let i = 0; i < message.length; i++) {
+    if (currmess.length < 999) {
+      currmess += message[i]
     }
-    if (currmess.length > 0) {
+    else {
       messagearr.push(currmess)
+      currmess = ""
     }
-    
-    // Iterate through array as mentions or regular post
-    for (let i = 0; i < messagearr.length; i++) {
-      // Send message(s) w/ mention(s)
-      if (mentionids) {
-        console.log(`Creating new mention (${messagearr[i].length}): ${messagearr[i]}`)
-        let text = messagearr[i].replace("/", "@")
-        var payload = {
-            text,
-            bot_id,
-            attachments: [{ loci: [], type: "mentions", user_ids: [] }]
-          }
-        
-          for (let i = 0; i < mentionids.length; i++) {
-            payload.attachments[0].loci.push([0, messagearr[i].length])
-            payload.attachments[0].user_ids.push(mentionids[i])
-        }
+  }
+  if (currmess.length > 0) {
+    messagearr.push(currmess)
+  }
 
-        console.log(`Mentioning: ${payload.attachments[0].user_ids}`)
-
-        // Prep message as JSON and construct packet
-        const json = JSON.stringify(payload)
-        const groupmeAPIOptions = {
-          agent: false,
-          host: "api.groupme.com",
-          path: "/v3/bots/post",
-          port: 443,
-          method: "POST",
-          headers: {
-            "Content-Length": json.length,
-            "Content-Type": "application/json",
-            "X-Access-Token": accesstoken
-          }
-        }
-
-        const req = https.request(groupmeAPIOptions, response => {
-          let data = ""
-          response.on("data", chunk => (data += chunk))
-          response.on("end", () =>
-            console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-          )
-        })
-        req.end(json)
+  // Iterate through array as mentions or regular post
+  for (let i = 0; i < messagearr.length; i++) {
+    // Send message(s) w/ mention(s)
+    if (mentionids) {
+      console.log(`Creating new mention (${messagearr[i].length}): ${messagearr[i]}`)
+      let text = messagearr[i].replace("/", "@")
+      var payload = {
+        text,
+        bot_id,
+        attachments: [{ loci: [], type: "mentions", user_ids: [] }]
       }
 
-      // Send regular message(s)
-      else {
-        var payload = {
-          "text": messagearr[i],
-          bot_id
-        }
-        var response = await got.post(desturl, {
-          json: payload
-        })
+      for (let i = 0; i < mentionids.length; i++) {
+        payload.attachments[0].loci.push([0, messagearr[i].length])
+        payload.attachments[0].user_ids.push(mentionids[i])
+      }
 
-        const statusCode = response.statusCode
-        if (statusCode !== 201) {
-          console.log(`Error creating a post ${statusCode}`)
+      console.log(`Mentioning: ${payload.attachments[0].user_ids}`)
+
+      // Prep message as JSON and construct packet
+      const json = JSON.stringify(payload)
+      const groupmeAPIOptions = {
+        agent: false,
+        host: "api.groupme.com",
+        path: "/v3/bots/post",
+        port: 443,
+        method: "POST",
+        headers: {
+          "Content-Length": json.length,
+          "Content-Type": "application/json",
+          "X-Access-Token": accesstoken
         }
       }
+
+      const req = https.request(groupmeAPIOptions, response => {
+        let data = ""
+        response.on("data", chunk => (data += chunk))
+        response.on("end", () =>
+          console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+        )
+      })
+      req.end(json)
     }
+
+    // Send regular message(s)
+    else {
+      var payload = {
+        "text": messagearr[i],
+        bot_id
+      }
+      var response = await got.post(desturl, {
+        json: payload
+      })
+
+      const statusCode = response.statusCode
+      if (statusCode !== 201) {
+        console.log(`Error creating a post ${statusCode}`)
+      }
+    }
+  }
 }
 
 // Send a DM to a provided user ID on host's behalf
-const sendDm = async (userid, slashtext) => {
-  console.log(`Creating new DM (${slashtext.length}): ${slashtext}`)
-  let text = slashtext.replace("/", "@")
+const sendDm = async (userid, text) => {
+  console.log(`Creating new DM (${text.length}): ${text}`)
   const recipient_id = userid
-  const source_guid = String(Math.random().toString(36).substring(2,34))
+  const source_guid = String(Math.random().toString(36).substring(2, 34))
 
-  const message = {
-    direct_message : {
-      recipient_id,
-      source_guid,
-      text
+  // Prep message as array to accomadate long messages 
+  let messagearr = []
+  var currmess = ""
+  for (let i = 0; i < text.length; i++) {
+    if (currmess.length < 999) {
+      currmess += text[i]
+    }
+    else {
+      messagearr.push(currmess)
+      currmess = ""
     }
   }
-
-  // Prep message as JSON and construct packet
-  const json = JSON.stringify(message)
-  const groupmeAPIOptions = {
-    agent: false,
-    host: "api.groupme.com",
-    path: "/v3/direct_messages",
-    port: 443,
-    method: "POST",
-    headers: {
-      "Content-Length": json.length,
-      "Content-Type": "application/json",
-      "X-Access-Token": accesstoken
-    }
+  if (currmess.length > 0) {
+    messagearr.push(currmess)
   }
 
-  // Send request
-  const req = https.request(groupmeAPIOptions, response => {
-    let data = ""
-    response.on("data", chunk => (data += chunk))
-    response.on("end", () =>
-      console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-    )
-  })
-  req.end(json)
+  for (let i = 0; i < messagearr.length; i++) {
+    const message = {
+      direct_message: {
+        recipient_id,
+        source_guid,
+        "text" : messagearr[i]
+      }
+    }
+
+    // Prep message as JSON and construct packet
+    const json = JSON.stringify(message)
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/direct_messages",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "application/json",
+        "X-Access-Token": accesstoken
+      }
+    }
+
+    // Send request
+    const req = https.request(groupmeAPIOptions, response => {
+      let data = ""
+      response.on("data", chunk => (data += chunk))
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      )
+    })
+    req.end(json)
+  }
 
 }
 
@@ -167,14 +184,14 @@ const sendDm = async (userid, slashtext) => {
 const getBallers = async () => {
   const limit = 5
   const date = new Date().getTime()
-  const yesterdaylong = date - 24*60*60*1000
+  const yesterdaylong = date - 24 * 60 * 60 * 1000
   const yesterday = new Date(yesterdaylong)
   var end_at = yesterday.toISOString()
 
   const getpath = `/v3/conversations/${groupid}/events/list?end_at=${end_at}&limit=${limit}&token=${accesstoken}`
   const desturl = new URL(getpath, baseurl)
   const response = await got(desturl, {
-      responseType: "json"
+    responseType: "json"
   })
 
   console.log(response.body.response)
@@ -208,7 +225,7 @@ const getNewbies = async () => {
   const getpath = `/v3/groups/${groupid}/messages?limit=${limit}&token=${accesstoken}`
   const desturl = new URL(getpath, baseurl)
   const response = await got(desturl, {
-      responseType: "json"
+    responseType: "json"
   })
 
   // console.log(response.body.response)
@@ -241,7 +258,7 @@ const getAdmins = async () => {
   const getpath = `/v3/groups/${groupid}?token=${accesstoken}`
   const desturl = new URL(getpath, baseurl)
   const response = await got(desturl, {
-      responseType: "json"
+    responseType: "json"
   })
 
   // Get admin details
@@ -263,11 +280,11 @@ const getUserId = async (name) => {
   const getpath = `/v3/groups/${groupid}?token=${accesstoken}`
   const desturl = new URL(getpath, baseurl)
   const response = await got(desturl, {
-      responseType: "json"
+    responseType: "json"
   })
 
   let memberdict = response.body.response.members
-  
+
   for (const key of Object.entries(memberdict)) {
     if (key[1].nickname == name) {
       console.log(`Found ${name} with user id ${key[1].user_id}`)
@@ -278,41 +295,41 @@ const getUserId = async (name) => {
 }
 
 // Post pic from URL
-const postPic = async(text) => {
+const postPic = async (text) => {
   console.log(`Posting pic`)
   const message = {
-      text,
-      bot_id
-    }
+    text,
+    bot_id
+  }
 
-    // Prep message as JSON and construct packet
-    const json = JSON.stringify(message)
-    const groupmeAPIOptions = {
-      agent: false,
-      host: "api.groupme.com",
-      path: "/v3/bots/post",
-      port: 443,
-      method: "POST",
-      headers: {
-        "Content-Length": json.length,
-        "Content-Type": "image/jpeg",
-        "X-Access-Token": accesstoken
-      }
+  // Prep message as JSON and construct packet
+  const json = JSON.stringify(message)
+  const groupmeAPIOptions = {
+    agent: false,
+    host: "api.groupme.com",
+    path: "/v3/bots/post",
+    port: 443,
+    method: "POST",
+    headers: {
+      "Content-Length": json.length,
+      "Content-Type": "image/jpeg",
+      "X-Access-Token": accesstoken
     }
+  }
 
-    // Send request
-    const req = https.request(groupmeAPIOptions, response => {
-      let data = ""
-      response.on("data", chunk => (data += chunk))
-      response.on("end", () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      )
-    })
-    req.end(json)
+  // Send request
+  const req = https.request(groupmeAPIOptions, response => {
+    let data = ""
+    response.on("data", chunk => (data += chunk))
+    response.on("end", () =>
+      console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+    )
+  })
+  req.end(json)
 }
 
 // Create event
-const createEvent = async(name, loc) => {
+const createEvent = async (name, loc) => {
   console.log(`Creating ${name} event`)
 
   // Need to find the nearest specified day of week (0 == Sun, 6 == Sat)
@@ -328,7 +345,7 @@ const createEvent = async(name, loc) => {
 
   // If the adjusted date is in the past, add 7 days
   if (startdate < currentdate) {
-  	startdate.setDate(startdate.getDate() + 7)
+    startdate.setDate(startdate.getDate() + 7)
     enddate.setDate(enddate.getDate() + 7)
   }
 
@@ -341,48 +358,48 @@ const createEvent = async(name, loc) => {
   const end_at = enddate.toISOString()
 
   const message = {
-      name,
-      start_at,
-      end_at,
-      "is_all_day": false,
-      "timezone": "America/Detroit",
-      "location": {"name": loc}
-    }
+    name,
+    start_at,
+    end_at,
+    "is_all_day": false,
+    "timezone": "America/Detroit",
+    "location": { "name": loc }
+  }
 
-    // Prep message as JSON and construct packet
-    const json = JSON.stringify(message)
-    const groupmeAPIOptions = {
-      agent: false,
-      host: "api.groupme.com",
-      path: `/v3/conversations/${groupid}/events/create`,
-      port: 443,
-      method: "POST",
-      headers: {
-        "Content-Length": json.length,
-        "Content-Type": "application/json",
-        "X-Access-Token": accesstoken
-      }
+  // Prep message as JSON and construct packet
+  const json = JSON.stringify(message)
+  const groupmeAPIOptions = {
+    agent: false,
+    host: "api.groupme.com",
+    path: `/v3/conversations/${groupid}/events/create`,
+    port: 443,
+    method: "POST",
+    headers: {
+      "Content-Length": json.length,
+      "Content-Type": "application/json",
+      "X-Access-Token": accesstoken
     }
+  }
 
-    // Send request
-    const req = https.request(groupmeAPIOptions, response => {
-      let data = ""
-      response.on("data", chunk => (data += chunk))
-      response.on("end", () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      )
-    })
-    req.end(json)
+  // Send request
+  const req = https.request(groupmeAPIOptions, response => {
+    let data = ""
+    response.on("data", chunk => (data += chunk))
+    response.on("end", () =>
+      console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+    )
+  })
+  req.end(json)
 }
 
 // Returns all your bots and their info
 const getBots = async () => {
-    const grouppath = `/v3/bots?token=${accesstoken}`
-    const desturl = new URL(grouppath, baseurl)
-    const response = await got(desturl, {
-        responseType: "json"
-    })
-    console.log(response.body.response)
+  const grouppath = `/v3/bots?token=${accesstoken}`
+  const desturl = new URL(grouppath, baseurl)
+  const response = await got(desturl, {
+    responseType: "json"
+  })
+  console.log(response.body.response)
 }
 
 ////////// REGEX //////////
