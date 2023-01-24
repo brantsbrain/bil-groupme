36925c73bb7be09ca86666a1bd17b1844ad75e56
Brant Goings <bgraulph@gmail.com>
2023-01-24 15:30:31 -0500

    Add node-fetch

--- groupme-api.js
+++ groupme-api.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const got = require("got")
 const {URL} = require("url")
 const https = require("https")
+const fetch = require("node-fetch")
 
 ////////// INITIALIZE VARS //////////
 // Used to access GroupMe API
