af6ea4c4bc6b18aa879ea709275eea6da45e0cd0
Brant Goings <bgraulph@gmail.com>
2023-03-21 13:40:50 -0400

    Add help text for /cancel

--- groupme-api.js
+++ groupme-api.js
@@ -866,6 +866,7 @@ var helptext = `Bot Commands:\n` +
   `\nAdmin Commands:\n` +
   `/ballers [message] - Mention all people going to nearest upcoming event\n` +
   `/everyone [message] - Mention everyone in the group\n` +
+  `/cancel - Cancel nearest upcoming event (must be created by Brant)\n` +
 
   `\nNavigating GroupMe:\n` +
   `Responding to a poll - Click/Tap the group picture in the upper right corner, find 'Polls', and select and cast your vote(s) for the desired options\n` +
